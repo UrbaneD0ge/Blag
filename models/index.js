3bb366e1094dbbedcdbffb8e1e0f5912ba18fe32
Kip <88916382+UrbaneD0ge@users.noreply.github.com>
2021-10-26 09:55:28 -0400

    models

--- models/index.js
+++ models/index.js
@@ -1,1 +1,13 @@
-const Pokemon = require('./pokemon')
+const User = require('./User.js');
+const Post = require('./Post');
+
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+module.exports = { User, Post };
--- server.js
+++ server.js
@@ -1,11 +1,19 @@
-const express = require ('express');
-const exphbs = require ('express-handlebars');
-const sequelize = require ('./config/connection');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+const path = require('path');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({});
+// Handlebars custom helpers
+const hbs = exphbs.create({ helpers });
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
@@ -13,4 +21,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-sequelize.sync({ force: false }).then(() => ... )
+const sess = {
+  secret: 'Nobody needs to know',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening! 🎧 '));
+});
